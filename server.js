f9d62deb7798d8d7f88556245364e05370e49f70
Katya Rukosuev <Katyafox93@gmail.com>
2022-05-29 15:01:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+// const path = require('path')
 const PORT = process.env.PORT || 4578
-// app.use(express.static("client/build"))
-app.use(express.static(path.join(__dirname, 'client', 'build')));
+app.use(express.static("client/build"))
+// app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.json())
 
 const {DBConnect} = require('./DBConnect')
 
 DBConnect;
 
-app.get('/', express.static(path.join(__dirname, 'client', 'build')))
 app.listen(PORT, ()=>{
     console.log(`listen on http://localhost:${PORT}`)
 })
