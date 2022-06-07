f5f102313a904c872062070826eb858ec3d15cc0
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-06-07 20:39:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
-// const path = require('path')
 const PORT = process.env.PORT || 4578
 
 app.use(express.static("client/build"))
-// app.use(express.static(path.join(__dirname, 'client', 'build')));
 app.use(express.json())
 
 const { DBConnect } = require('./DBConnect')
