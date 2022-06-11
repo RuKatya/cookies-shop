d18065df216b8bdfc25c6e075cf898c59be77774
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-06-11 19:01:12 +0300

    navbar

--- client/src/View/Components/NavBar.jsx
+++ client/src/View/Components/NavBar.jsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import { IconButton, InputBase, Paper } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 
 const NavBar = ({ pageWidth }) => {
   console.log(pageWidth);
@@ -17,11 +21,25 @@ const NavBar = ({ pageWidth }) => {
         <Link to="/">Cookie-store &hearts;</Link>
       </div>
 
-      <Link to="cart">Cart</Link>
+      <Link to="cart">
+        <ShoppingCartOutlinedIcon />
+      </Link>
 
-      <div className="searchInput">
+      {/* <div className="searchInput">
+        <SearchOutlinedIcon className="searchInput__searchIcon" />
         <input type="text" name="searchCookie" placeholder="Search" />
-      </div>
+      </div> */}
+
+      <Paper
+        component="form"
+        className="searchInput"
+        sx={{ p: "1px 4px", display: "flex", alignItems: "center" }}
+      >
+        <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search" />
+        <IconButton>
+          <SearchIcon />
+        </IconButton>
+      </Paper>
 
       {(toggleMenu || pageWidth > 700) && (
         <div className="logReg">
